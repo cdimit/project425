e4b31cf90943b98eb90eb50f14ff1e31356484f2
jfu00001 <jfu00001@ucy.ac.cy>
2016-11-17 15:48:36 +0200

    routes

--- inClassEVoting/routes/web.php
+++ inClassEVoting/routes/web.php
@@ -11,13 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
 
 
